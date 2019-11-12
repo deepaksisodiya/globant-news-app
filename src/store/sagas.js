df2a3b26ab122a8afc32c670a5d21be977d643e0
Deepak Sisodiya <deepak.sisodiya@gmail.com>
2019-11-12 21:43:25 +0530

    added type.js

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,26 +1,27 @@
 import { call, put, takeEvery } from 'redux-saga/effects'
 
 import { getNews, getTopNews } from './../api';
+import * as constant from './type';
 
 function* fetchNews(action) {
   try {
     const news = yield call(getNews, action.payload);
-    yield put({ type: 'NEWS_FETCH_SUCCEEDED', payload: news.data });
+    yield put({ type: constant.NEWS_FETCH_SUCCEEDED, payload: news.data });
   } catch (e) {
-    yield put({ type: 'NEWS_FETCH_FAILED', message: e.message });
+    yield put({ type: constant.NEWS_FETCH_FAILED, message: e.message });
   }
 }
 
 function* fetchTopNews() {
   try {
     const topNews = yield call(getTopNews);
-    yield put({ type: 'TOPNEWS_FETCH_SUCCEEDED', payload: topNews });
+    yield put({ type: constant.TOPNEWS_FETCH_SUCCEEDED, payload: topNews });
   } catch (e) {
-    yield put({ type: 'TOPNEWS_FETCH_FAILED', message: e.message });
+    yield put({ type: constant.TOPNEWS_FETCH_FAILED, message: e.message });
   }
 }
 
 export default function* newsSaga() {
-  yield takeEvery('NEWS_FETCH_REQUESTED', fetchNews);
-  yield takeEvery('TOPNEWS_FETCH_REQUESTED', fetchTopNews);
+  yield takeEvery(constant.NEWS_FETCH_REQUESTED, fetchNews);
+  yield takeEvery(constant.TOPNEWS_FETCH_REQUESTED, fetchTopNews);
 }
